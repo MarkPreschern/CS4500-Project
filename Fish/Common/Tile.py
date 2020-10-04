115de2eb8afcb3908f7813cb21aafd6bc41b9fc3
drw <nedea.a@northeastern.edu>
2020-10-04 16:37:41 -0400

    initial commit

---
+++ Fish/Common/Tile.py
@@ -0,0 +1,23 @@
+import random
+import constants as ct
+from AbstractTile import AbstractTile
+
+
+class Tile(AbstractTile):
+    """
+    Represents a tile in the game onto which fish rest.
+    """
+    def __init__(self, fish_no):
+        # Validate params
+        if not isinstance(fish_no, int) or fish_no < 1:
+            raise ValueError('Invalid fish_no - expected a positive integer!')
+
+        # Set # of fish
+        self.__fish_no = fish_no
+
+    @property
+    def fish_no(self):
+        """
+        Retrieves the number of fish to the tile.
+        """
+        return self.__fish_no
