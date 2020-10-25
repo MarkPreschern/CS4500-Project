600c8f0f5d52b7fb320a44fb8541c552ff440afd
jkemly <kemly.j@northeastern.edu>
2020-10-25 18:42:01 -0400

    initialize xtree and start to refactor; add test changes

---
+++ 5/Other/xtree.py
@@ -0,0 +1,258 @@
+import sys
+import json
+
+sys.path.append("../3/Other")
+sys.path.append("../Fish/Common")
+
+from xboard import initialize_board
+from player import Player
+from board import Board
+from movement_direction import MovementDirection
+from color import Color
+from action import Action
+from exceptions.InvalidActionException import InvalidActionException
+from exceptions.GameNotRunningException import GameNotRunningException
+from game_tree import GameTree
+from position import Position
+from state import State
+
+directions_to_try = [MovementDirection.Top, MovementDirection.TopRight, MovementDirection.BottomRight,
+                     MovementDirection.Bottom, MovementDirection.BottomLeft, MovementDirection.TopLeft]
+
+
+def xtree() -> None:
+    """
+    Reads in a game state from STDIN and writes the next game state to STDOUT.
+    The next game state is the one resulting from the first player's first penguin moving
+    in the first permissible direction. If said penguin cannot move in any direction then
+    False is written to STDOUT instead.
+    """
+
+    # Initialize objects to read to
+    input_obj = ""
+
+    # Read from STDIN indefinitely until stream is closed
+    for k in sys.stdin:
+        input_obj += k
+
+    # Load from read string
+    json_obj = json.loads(input_obj)
+
+    # json_obj now has a state, a from position, and a to position. Get state from json
+    state = _get_next_state(json_obj['state'], json_obj['from'], json_obj['to'])
+
+    # If there is no next state for first avatar, print False
+    if state is None:
+        print(json.dumps(False))
+    else:
+        # Write JSON to STDOUT
+        print(json.dumps(_state_to_json(state)))
+
+
+def _board_to_json(board: Board) -> []:
+    """
+    Converts given Board object to an array, where
+    each entry is an array that represents a row on the board.
+    Each said array is an ordered series of numbers that represents
+    the number of fish on the tiles.
+
+    :param board: Board object to parse
+    :return: resulting array
+    """
+    if not isinstance(board, Board):
+        raise TypeError('Expected Board for board!')
+
+    # Create two dimensional matrix as a place holder with the
+    # right dimensions
+    matrix = []
+
+    # Create place holders in matrix
+    for i in range(board.rows):
+        matrix.append([])
+        for j in range(board.cols):
+            matrix[i].append(0)
+
+    # Cycle over tiles and set items in matrix
+    for pos, tile in board.tiles.items():
+        matrix[pos[0]][pos[1]] = tile.fish_no
+
+    return matrix
+
+
+def _state_to_json(state: State) -> dict:
+    """
+    Produces JSON for given state.
+
+    :param state: state to produce JSON for
+    :return: resulting json dictionary
+    """
+    # Check params
+    if not isinstance(state, State):
+        raise TypeError('Expected State object or None!')
+
+    json_obj = dict()
+
+    json_obj['board'] = _board_to_json(state.board)
+    json_obj['players'] = []
+
+    player_order = state.get_player_order()
+
+    # Cycle over player order
+    for player_id in player_order:
+        # Initialize dictionary in which to store player data
+        player_obj = {}
+
+        # Gather player data
+        player_obj['score'] = state.get_player_score(player_id)
+        player_obj['places'] = [[pos.x, pos.y] for pos in state.get_player_positions(player_id)]
+        player_obj['color'] = state.get_player_color(player_id).name.lower()
+
+        # Append player object to list
+        json_obj['players'].append(player_obj)
+
+    return json_obj
+
+
+def _str_to_color(color_str: str) -> Color:
+    """
+    Converts a string color to a Color object.
+
+    :param color_str: string to parse
+    :return: Color object
+    """
+    if not isinstance(color_str, str):
+        raise TypeError('Expected str for color_str!')
+
+    # Capitalize
+    color_str = color_str.upper()
+
+    # Make sure color exists
+    if color_str not in Color.__dict__:
+        raise ValueError('Not such color!')
+
+    return Color[color_str]
+
+
+def _get_next_state(json_obj: dict, from_pos: list, to_pos: list) -> State:
+    """
+    Produces the subsequent game state from trying to move first player's
+    first avatar in an order of directions. If said avatar cannot go
+    in any of the specified directions, None is returned.
+
+    :param json_obj: json object (dictionary) containing player list and board
+    :return: resulting state or False.
+    """
+    if not isinstance(json_obj, dict):
+        raise TypeError('Expected dict for json_obj!')
+
+    if 'players' not in json_obj.keys():
+        raise ValueError('Expected players in object!')
+
+    if 'board' not in json_obj.keys():
+        raise ValueError('Expected board in object!')
+
+    # Retrieve player list
+    player_list = json_obj['players']
+    board_json = json_obj['board']
+
+    # Get board from json
+    board = initialize_board(board_json)
+
+    # Initialize counter to generate player ids
+    player_id_counter = 1
+
+    # Initialize empty collection to hold players
+    players = []
+
+    # Initialize collection to hold player ids and their
+    # avatar placements
+    player_placements = {}
+
+    # Cycle over each json object in the json list
+    for player in player_list:
+        # Make up Player object
+        players.append(Player(player_id_counter, "", player_id_counter,
+                              _str_to_color(player['color'])))
+        # Insert placement
+        player_placements.update({player_id_counter: player['places']})
+        # Increment id counter
+        player_id_counter += 1
+
+    # Make up state with board and players
+    state = State(board, players)
+
+    # Extract first player's poisitions from json
+    first_avatar_pos = player_placements[1][0]
+    second_avatar_pos = player_placements[1][0]
+
+    # Convert first player's positions to Position objects
+    position_1 = Position(first_avatar_pos[0], first_avatar_pos[1])
+    position_2 = Position(second_avatar_pos[0], second_avatar_pos[1])
+
+    # Determine the number of avatars per player
+    avatars_per_player_no = len(player_placements[1])
+
+    # For each i in the number of avatars per player
+    for i in range(avatars_per_player_no):
+        # For each player, place i-th avatar
+        for p_id, p_placements in player_placements.items():
+            # Retrieve current player's i-th avatar
+            placement = player_placements[p_id][i]
+            # Convert to Position object
+            position_to_place = Position(placement[0], placement[1])
+
+            # Place current player's avatar at position
+            state.place_avatar(position_to_place)
+
+    # Attempt to move in a number of directions
+    for direction in directions_to_try:
+        # first_avatar_pos
+        next_pos = _get_next_position(first_avatar_pos, direction)
+
+        try:
+            # Try action on game state
+            new_state = GameTree.try_action(GameTree(state), Action(first_avatar_pos, next_pos))
+            return new_state
+        except InvalidActionException:
+            pass
+        except GameNotRunningException:
+            return None
+
+    return None
+
+
+def _get_next_position(position: Position, direction: MovementDirection):
+    """
+    Retrieves the subsequent position from the given position one step
+    in the given direction.
+
+    :param position: position to start from
+    :param direction: direction to go in
+    """
+    # Validate params
+    if not isinstance(position, Position):
+        raise TypeError('Expected Position object for position!')
+
+    if not isinstance(direction, MovementDirection):
+        raise TypeError('Expected MovementDirection for direction!')
+
+    if direction == MovementDirection.Top:
+        next_position = Position(position.x - 2, position.y)
+    elif direction == MovementDirection.Bottom:
+        next_position = Position(position.x + 2, position.y)
+    elif direction == MovementDirection.TopLeft:
+        next_y = (position.y - 1) if position.x % 2 == 0 else position.y
+        next_position = Position(position.x - 1, next_y)
+    elif direction == MovementDirection.TopRight:
+        next_y = position.y if position.x % 2 == 0 else position.y + 1
+        next_position = Position(position.x - 1, next_y)
+    elif direction == MovementDirection.BottomRight:
+        next_y = position.y if position.x % 2 == 0 else position.y + 1
+        next_position = Position(position.x + 1, next_y)
+    elif direction == MovementDirection.BottomLeft:
+        next_y = (position.y - 1) if position.x % 2 == 0 else position.y
+        next_position = Position(position.x + 1, next_y)
+    else:
+        next_position = -1
+
+    return next_position
---
+++ 5/Other/xtree_tests.py
@@ -0,0 +1,233 @@
+import sys
+import unittest
+
+
+sys.path.append("../Fish/Common")
+
+from board import Board
+from position import Position
+from tile import Tile
+from xstate import _board_to_json, _state_to_json, _str_to_color, _get_next_position, _get_next_state
+from hole import Hole
+from state import State
+from color import Color
+from player import Player
+from movement_direction import MovementDirection
+
+
+class XStateTests(unittest.TestCase):
+    def __init__(self, *args, **kwargs):
+        super(XStateTests, self).__init__(*args, **kwargs)
+
+        Board.DISABLE_SPRITE_MANAGER = True
+
+    def test_board_to_json_fail1(self):
+        # Fails due to invalid board
+
+        with self.assertRaises(TypeError):
+            _board_to_json('')
+
+    def test_board_to_json_success1(self):
+        # Tests successful board to json
+        b = Board.homogeneous(2, 4, 2)
+
+        self.assertSequenceEqual(_board_to_json(b),
+                                 [[2, 2], [2, 2], [2, 2], [2, 2]])
+
+    def test_board_to_json_success2(self):
+        # Tests successful board to json
+        b = Board({
+            Position(0, 0): Tile(5),
+            Position(0, 1): Tile(3),
+            Position(0, 2): Tile(2),
+            Position(1, 0): Hole(),
+            Position(1, 1): Hole(),
+            Position(1, 2): Tile(2),
+            Position(2, 0): Tile(3),
+            Position(2, 1): Tile(4),
+            Position(2, 2): Hole(),
+            Position(3, 0): Hole(),
+            Position(3, 1): Tile(1),
+            Position(3, 2): Tile(5)
+        })
+
+        self.assertSequenceEqual(_board_to_json(b),
+                                 [[5, 3, 2], [0, 0, 2], [3, 4, 0], [0, 1, 5]])
+
+    def test_state_to_json_fail1(self):
+        # Fails due to state provided not being State
+        with self.assertRaises(TypeError):
+            _state_to_json('blah')
+
+    def test_state_to_json_success1(self):
+        # Successful state to json. State is one in which
+        # all avatars have been placed
+        state = State(Board.homogeneous(4, 4, 3), [
+            Player(1, 'bob', 10, Color.WHITE),
+            Player(2, 'bob', 8, Color.BROWN),
+            Player(3, 'bob', 12, Color.BLACK)
+        ])
+
+        # Player 1 place
+        state.place_avatar(Position(0, 1))
+        # Player 2 place
+        state.place_avatar(Position(1, 0))
+        # Player 3 place
+        state.place_avatar(Position(1, 1))
+        # Player 1 place
+        state.place_avatar(Position(1, 2))
+        # Player 2 place
+        state.place_avatar(Position(2, 0))
+        # Player 3 place
+        state.place_avatar(Position(2, 1))
+        # Player 1 place
+        state.place_avatar(Position(2, 2))
+        # Player 2 place
+        state.place_avatar(Position(3, 1))
+        # Player 3 place
+        state.place_avatar(Position(3, 2))
+
+        # Get jsonified version
+        result = _state_to_json(state)
+
+        # Make up expected json
+        expected_json = {
+            'players': [
+                {'score': 0, 'places': [[0, 1], [1, 2], [2, 2]], 'color': 'brown'},
+                {'score': 0, 'places': [[1, 0], [2, 0], [3, 1]], 'color': 'white'},
+                {'score': 0, 'places': [[1, 1], [2, 1], [3, 2]], 'color': 'black'}
+            ],
+            'board': [[4, 4, 4], [4, 4, 4], [4, 4, 4], [4, 4, 4]]
+        }
+
+        self.assertEqual(result, expected_json)
+
+    def test_state_to_json_success2(self):
+        # Successful state to json. State is one in which
+        # only some avatars have been placed
+        state = State(Board.homogeneous(4, 4, 3), [
+            Player(1, 'bob', 10, Color.WHITE),
+            Player(2, 'bob', 8, Color.BROWN),
+            Player(3, 'bob', 12, Color.BLACK)
+        ])
+
+        # Player 1 place
+        state.place_avatar(Position(0, 1))
+        # Player 2 place
+        state.place_avatar(Position(1, 0))
+        # Player 3 place
+        state.place_avatar(Position(1, 1))
+        # Player 1 place
+        state.place_avatar(Position(1, 2))
+        # Player 2 place
+        state.place_avatar(Position(2, 0))
+        # Player 3 place
+        state.place_avatar(Position(2, 1))
+        # Player 1 place
+        state.place_avatar(Position(2, 2))
+
+        # Get jsonified version
+        result = _state_to_json(state)
+
+        # Make up expected json
+        expected_json = {
+            'players': [
+                {'score': 0, 'places': [[1, 0], [2, 0]], 'color': 'white'},
+                {'score': 0, 'places': [[1, 1], [2, 1]], 'color': 'black'},
+                {'score': 0, 'places': [[0, 1], [1, 2], [2, 2]], 'color': 'brown'}
+            ],
+            'board': [[4, 4, 4], [4, 4, 4], [4, 4, 4], [4, 4, 4]]
+        }
+
+        self.assertEqual(result, expected_json)
+
+    def test_state_to_json_success3(self):
+        # Successful state to json. State is one in which
+        # no avatars have been placed
+        state = State(Board.homogeneous(4, 4, 3), [
+            Player(1, 'bob', 10, Color.WHITE),
+            Player(2, 'bob', 8, Color.BROWN),
+            Player(3, 'bob', 12, Color.BLACK)
+        ])
+
+        # Get jsonified version
+        result = _state_to_json(state)
+
+        # Make up expected json
+        expected_json = {
+            'players': [
+                {'score': 0, 'places': [], 'color': 'brown'},
+                {'score': 0, 'places': [], 'color': 'white'},
+                {'score': 0, 'places': [], 'color': 'black'}
+            ],
+            'board': [[4, 4, 4], [4, 4, 4], [4, 4, 4], [4, 4, 4]]
+        }
+
+        self.assertEqual(result, expected_json)
+
+    def test_str_to_color_fail1(self):
+        # Fails due to invalid color_str
+        with self.assertRaises(TypeError):
+            _str_to_color(214)
+
+    def test_str_to_color_fail2(self):
+        # Fails due to color not existing
+        with self.assertRaises(ValueError):
+            _str_to_color("gray")
+
+    def test_str_to_color_success(self):
+        # Tests a series of successful color
+        # conversions
+
+        self.assertEqual(_str_to_color("red"), Color.RED)
+        self.assertEqual(_str_to_color("white"), Color.WHITE)
+        self.assertEqual(_str_to_color("brown"), Color.BROWN)
+        self.assertEqual(_str_to_color("black"), Color.BLACK)
+
+    def test_get_next_position_fail1(self):
+        # Fails due to position being invalid
+        with self.assertRaises(TypeError):
+            _get_next_position('bla', MovementDirection.Top)
+
+    def test_get_next_position_fail2(self):
+        # Fails due to movement direction being invalid
+        with self.assertRaises(TypeError):
+            _get_next_position(Position(0, 0), 'bla')
+
+    def test_get_next_position_fail3(self):
+        # Fails due to both invalid being invalid
+        with self.assertRaises(TypeError):
+            _get_next_position('suh', 'bla')
+
+    def test_get_next_position_success1(self):
+        # Tests a series of successful _get_next_position
+
+        self.assertEqual(_get_next_position(Position(0, 0), MovementDirection.BottomRight),
+                         Position(1, 0))
+        self.assertEqual(_get_next_position(Position(0, 0), MovementDirection.TopLeft),
+                         Position(-1, -1))
+        self.assertEqual(_get_next_position(Position(0, 0), MovementDirection.Bottom),
+                         Position(2, 0))
+        self.assertEqual(_get_next_position(Position(4, 2), MovementDirection.Top),
+                         Position(2, 2))
+        self.assertEqual(_get_next_position(Position(4, 2), MovementDirection.TopLeft),
+                         Position(3, 1))
+        self.assertEqual(_get_next_position(Position(4, 2), MovementDirection.BottomLeft),
+                         Position(5, 1))
+        self.assertEqual(_get_next_position(Position(4, 2), MovementDirection.TopRight),
+                         Position(3, 2))
+
+    def test_get_next_state_fail1(self):
+        # Fails due to json_obj being invalid
+        with self.assertRaises(TypeError):
+            _get_next_state('')
+
+    def test_get_next_state_fail2(self):
+        # Fails due to json_obj not containing players
+        with self.assertRaises(ValueError):
+            _get_next_state({'board': []})
+
+    def test_get_next_state_fail3(self):
+        # Fails due to json_obj not containing board
+        with self.assertRaises(ValueError):
+            _get_next_state({'players': []})
