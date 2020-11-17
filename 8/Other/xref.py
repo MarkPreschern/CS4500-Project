973e132fc1decf7adf5aa54825415fb1a3fc5a20
MarkPreschern <mark.preschern@gmail.com>
2020-11-16 19:27:22 -0500

    Completed draft for Milestone 8 testing task

--- 8/Other/xref.py
+++ 8/Other/xref.py
@@ -2,12 +2,11 @@ import json
 import sys
 
 sys.path.append("../Fish/Player")
-sys.path.append("../4/Other")
-sys.path.append("../5/Other")
+sys.path.append("../Fish/Admin")
+sys.path.append("../Fish/")
 
-from xstate import initialize_state, _get_next_position, _str_to_color
-from xtree import _action_to_json
-from strategy import Strategy
+from player import Player
+from referee import Referee
 
 
 def xref():
@@ -21,29 +20,22 @@ def xref():
     # Load from read string
     json_obj = json.loads(input_obj)
 
-    # Get row, column, players, and fish from json_obj
-    row = json_obj['row']
-    column = json_obj['column']
+    # Get rows, columns, players, and fish from json_obj
+    rows = json_obj['row']
+    columns = json_obj['column']
     fish = json_obj['fish']
     players = []
 
     # initialize players;
     for p in json_obj['players']:
-        strategy player = Strategy
-        players.append()
+        players.append(Player(p[0], p[1]))
 
-    # Find the first player in the json state
-    first_player_color = _str_to_color(state['players'][0]['color'])
+    # Create Referee
+    referee = Referee(rows, columns, players, fish)
+    Referee.DIFFICULTY_FACTOR = 0
 
-    # Load the json state into a State object
-    initialized_state = initialize_state(state)
+    # Run complete game
+    referee.start()
 
-    # Check if the first player in the array is stuck and print false to STDOUT if so
-    if first_player_color in initialized_state.stuck_players:
-        print(json.dumps(False))
-    else:
-        # Compute the best action the first player can take given the current state and depth
-        best_action = Strategy.get_best_action(initialized_state, depth)
-
-        # Print the best action to STDOUT
-        print(_action_to_json(best_action))
+    # Print referee winners
+    print(map(lambda winner: winner.name(), referee.winners()))
